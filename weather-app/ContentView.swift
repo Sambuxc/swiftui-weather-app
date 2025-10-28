5cb891729b26ad630182636f7e4b96fde5448e80
Sam Bruton <samwbru@gmail.com>
2025-10-28 21:57:38 +0000

    Adds color asset

--- weather-app/ContentView.swift
+++ weather-app/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(
-                colors: [.pink, .blue, .white],
+                colors: [.pink, .blue, .lightBlue],
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             ).edgesIgnoringSafeArea(.all)  // applies background to entire screen
@@ -21,46 +21,47 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding(.top)
-
+                
                 VStack(spacing: 8) {
                     Image(systemName: "tornado")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 150, height: 150)
-
+                    
                     Text("76°C")
                         .font(.system(size: 70, weight: .thin))
                         .foregroundColor(.white)
                 }
                 Spacer()
-            }
-            HStack(spacing: 20) {
-                WeatherDayView(
-                    dayOfWeek: "tues",
-                    imageName: "sunrise.fill",
-                    temp: 12
-                )
-                WeatherDayView(
-                    dayOfWeek: "wed",
-                    imageName: "cloud.hail.fill",
-                    temp: 17
-                )
-                WeatherDayView(
-                    dayOfWeek: "thu",
-                    imageName: "sunset.fill",
-                    temp: 15
-                )
-                WeatherDayView(
-                    dayOfWeek: "fri",
-                    imageName: "wind.snow",
-                    temp: 9
-                )
-                WeatherDayView(
-                    dayOfWeek: "sat",
-                    imageName: "moon.stars.fill",
-                    temp: 11
-                )
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(
+                        dayOfWeek: "tues",
+                        imageName: "sunrise.fill",
+                        temp: 12
+                    )
+                    WeatherDayView(
+                        dayOfWeek: "wed",
+                        imageName: "cloud.hail.fill",
+                        temp: 17
+                    )
+                    WeatherDayView(
+                        dayOfWeek: "thu",
+                        imageName: "sunset.fill",
+                        temp: 15
+                    )
+                    WeatherDayView(
+                        dayOfWeek: "fri",
+                        imageName: "wind.snow",
+                        temp: 9
+                    )
+                    WeatherDayView(
+                        dayOfWeek: "sat",
+                        imageName: "moon.stars.fill",
+                        temp: 11
+                    )
+                }
             }
         }
     }
