68d1de91b04b855208b71b0882c158853d90c892
Samuel Bruton <samuel.bruton@ca.vu>
2025-10-28 12:34:08 +0000

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Samuel Bruton on 28/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
